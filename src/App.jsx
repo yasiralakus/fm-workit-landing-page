f68d7d873180b0986047153797cd05a0a2d1ec35
yasiralakus <yasiralakus11@gmail.com>
2025-03-10 23:08:47 +0300

    project ready for live

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,74 @@ export default function App() {
     return (
         <div className="full-page">
 
-            <div className="hero"></div>
+            <div className="hero">
+
+                <div className="header">
+
+                    <img src="./images/logo-light.svg" alt="" />
+
+                    <p>Apply for access</p>
+
+                </div>
+
+                <div className="data-tailored">
+
+                    <img className="bg-pattern-1" src="./images/bg-pattern-1.svg" alt="" />
+
+                    <img className="bg-pattern-2" src="./images/bg-pattern-2.svg" alt="" />
+
+                    <h1>Data <span>tailored</span> to your needs.</h1>
+
+                    <a href="">Learn more</a>
+
+                    <img src="./images/image-hero.webp" alt="" className="phone" />
+
+                </div>
+
+            </div>
+
+            <div className="number-section">
+
+                <div className="number">
+                    <h2>1</h2>
+                    <h1>Actionable insights</h1>
+                    <p>Optimize your products, improve customer satisfaction and stay ahead of the competition with our product data analytics.</p>
+                </div>
+
+                <div className="number">
+                    <h2>2</h2>
+                    <h1>Data-driven decisions</h1>
+                    <p>Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock insights hidden in your product data.</p>
+                </div>
+
+                <div className="number">
+                    <h2>3</h2>
+                    <h1>Always affordable</h1>
+                    <p>Always affordable pricing that scales with your business. Get top-quality product data analytics services without hidden costs or unexpected fees.</p>
+                </div>
+
+            </div>
+
+            <div className="user-section">
+                <img src="./images/image-founder.webp" alt="" />
+                <div className="user-content">
+                    <h1>Be the first to test</h1>
+                    <p>Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I’ll be in touch to schedule a call.</p>
+                    <a href="">Apply for access</a>
+                    <img className="bg-pattern-3" src="./images/bg-pattern-3.svg" alt="" />
+                </div>
+            </div>
+
+            <div className="footer">
+                <div className="footer-box">
+                    <img src="./images/logo-dark.svg" alt="" />
+                    <div className="socials">
+                        <img src="./images/icon-facebook.svg" alt="" />
+                        <img src="./images/icon-twitter.svg" alt="" />
+                        <img src="./images/icon-instagram.svg" alt="" />
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
